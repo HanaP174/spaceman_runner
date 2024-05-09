5ea57d3c0cb5e47b5129a8c07a99a1a0dc1edeab
HanaP174 <61006773+HanaP174@users.noreply.github.com>
2024-05-09 21:40:52 +0200

    spaceman running and jumping on place

--- spaceman/Spaceman.cs
+++ spaceman/Spaceman.cs
@@ -3,36 +3,46 @@ using System;
 
 public partial class Spaceman : CharacterBody2D
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+	// public const float Speed = 300.0f;
+	// public const float JumpVelocity = -400.0f;
 
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
-	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+	private const float Gravity = 4200f;
+	private const float JumpSpeed = -1800f;
+	private AnimatedSprite2D animatedSprite;
+
+	public override void _Ready()
+	{
+		animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSpaceman");
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
 
-		// Add the gravity.
-		if (!IsOnFloor())
-			velocity.Y += gravity * (float)delta;
-
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-			velocity.Y = JumpVelocity;
-
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
+		velocity.Y += Gravity * (float)delta;
+		if (IsOnFloor())
 		{
-			velocity.X = direction.X * Speed;
+			animatedSprite.Play("run");
+			if (Input.IsActionJustPressed("ui_accept"))
+				velocity.Y = JumpSpeed;
 		}
 		else
 		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			animatedSprite.Play("jump");
 		}
 
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		// Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		// if (direction != Vector2.Zero)
+		// {
+		// 	velocity.X = direction.X * Speed;
+		// }
+		// else
+		// {
+		// 	velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+		// }
+		//
 		Velocity = velocity;
 		MoveAndSlide();
 	}
